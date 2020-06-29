f51a821f281418607b291a1813f2ac3d81a38ed6
Patrycja Zamkowska <zamkowska.patrycja@gmail.com>
2020-06-30 00:14:33 +0200

    add employeescontroller with get test

---
+++ pjatk_api-employees/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pjatk_api_employees.Controllers
+{
+    // localhost:52269/api/employees
+    [Route("api/employees")] // adres pod jakim dostajemy się do naszego kontrolera
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        // localhost:52269/api/employees/test 
+        // metoda do sprawdzenia, czy api działa 
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("It works!");
+        }
+    }
+}
