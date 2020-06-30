7ba1038aeb4ab55e05db6d417a4cfb7b29c2d88b
Patrycja Zamkowska <zamkowska.patrycja@gmail.com>
2020-06-30 02:59:17 +0200

    specify service in startup

--- pjatk_api-employees/Startup.cs
+++ pjatk_api-employees/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using pjatk_api_employees.DAL;
 
 namespace pjatk_api_employees
 {
@@ -24,6 +25,7 @@ namespace pjatk_api_employees
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IExampleService, EntityFrameworkExampleService>();
             services.AddControllers();
         }
 
